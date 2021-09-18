f74bee36779e79ddd2316da12241cbc4304d2710
ravikiran300 <54069290+ravikiran300@users.noreply.github.com>
2021-09-18 20:05:51 +0530

    Create nginx-controller.sh

---
+++ nginx-controller.sh
@@ -0,0 +1,12 @@
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+helm install ingress-nginx ingress-nginx/ingress-nginx
+
+POD_NAME=$(kubectl get pods -l app.kubernetes.io/name=ingress-nginx -o jsonpath='{.items[0].metadata.name}')
+kubectl exec -it $POD_NAME -- /nginx-ingress-controller --version
+
+
+kubectl -n kubernetes-dashboard get svc
+
+kubectl -n default edit  svc ingress-nginx-controller
